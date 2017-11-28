9e62b4b79ec0b9b57772fba9d0d052de690781d0
David Parrott <david.parrott@gmail.com>
2017-11-28 15:48:34 +0000

    Add functionality from previous emulator projects into a utility library

---
+++ elbemu_utils/GdiRenderer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+using static elbemu_utils.NativeMethods;
+
+namespace elbemu_utils
+{
+    public class GdiRenderer
+    {
+        private Control _renderControl;
+
+        public GdiRenderer(Control renderControl)
+        {
+            _renderControl = renderControl;
+        }
+
+        public void DrawBitmap(Bitmap bitmap)
+        {
+            using (Graphics grDest = Graphics.FromHwnd(_renderControl.Handle))
+            using (Graphics grSrc = Graphics.FromImage(bitmap))
+            {
+                IntPtr hdcDest = IntPtr.Zero;
+                IntPtr hdcSrc = IntPtr.Zero;
+                IntPtr hBitmap = IntPtr.Zero;
+                IntPtr hOldObject = IntPtr.Zero;
+
+                try
+                {
+                    hdcDest = grDest.GetHdc();
+                    hdcSrc = grSrc.GetHdc();
+                    hBitmap = bitmap.GetHbitmap();
+
+                    hOldObject = Gdi32.SelectObject(hdcSrc, hBitmap);
+                    if (hOldObject == IntPtr.Zero)
+                        throw new Win32Exception();
+                    if (!Gdi32.StretchBlt(hdcDest, 0, 0, _renderControl.Width, _renderControl.Height,
+                                            hdcSrc, 0, 0, bitmap.Width, bitmap.Height,
+                                            Gdi32.TernaryRasterOperations.SRCCOPY))
+                        throw new Win32Exception();
+                }
+                finally
+                {
+                    if (hOldObject != IntPtr.Zero) Gdi32.SelectObject(hdcSrc, hOldObject);
+                    if (hBitmap != IntPtr.Zero) Gdi32.DeleteObject(hBitmap);
+                    if (hdcDest != IntPtr.Zero) grDest.ReleaseHdc(hdcDest);
+                    if (hdcSrc != IntPtr.Zero) grSrc.ReleaseHdc(hdcSrc);
+                }
+            }
+        }
+    }
+}
---
+++ elbemu_utils/MessagePump.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace elbemu_utils
+{
+    public class MessagePump
+    {
+        private  Action _idleLoop;
+        private  bool _initialised;
+        private  bool _running;
+
+        private  bool ApplicationStillIdle => !NativeMethods.User32.PeekMessage(out _, IntPtr.Zero, 0, 0, 0);
+
+        public  void RunWhileIdle(Action idleLoop)
+        {
+            _idleLoop = idleLoop;
+            _initialised = true;
+            _running = true;
+
+            Application.Idle += OnIdle;
+        }
+
+        public  void Pause() => _running = false;
+
+        public  void Resume() => _running = _initialised && true;
+
+        public  void Stop()
+        {
+            if (_initialised)
+            {
+                _running = false;
+                _initialised = false;
+
+                Application.Idle -= OnIdle;
+            }
+        }
+        
+        private  void OnIdle(object sender, EventArgs e)
+        {
+            while (_running && ApplicationStillIdle)
+            {
+                _idleLoop();
+            }
+        }
+    }
+}
